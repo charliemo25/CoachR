ec39c005d8198a2bdca145804b445b4c8f292e4c
Moreau Charlie <moreaucharlie25@gmail.com>
2022-02-08 10:33:26 +0100

    WIP: ApiController

--- back/src/Controller/ApiController.php
+++ back/src/Controller/ApiController.php
@@ -2,18 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\MakerBundle\Validator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
+#[Route('/api', name: 'api')]
 class ApiController extends AbstractController
 {
-    #[Route('/api', name: 'api')]
-    public function index(): Response
+    public function __construct(
+        private EntityManagerInterface $em,
+        private UserRepository $userRepository,
+        private RequestStack $requestStack,
+        private SerializerInterface $serializer
+    ) 
+    {}
+
+    #[Route('/users', name:'create_user', methods:['POST'])]
+    public function createUser(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ApiController.php',
-        ]);
+        $request = $this->requestStack->getCurrentRequest();
+
+        $user = $this->serializer->deserialize($request->getContent(), User::class, 'json');
+        $errors = $validator->validate();
+
+        return $this->json("L'utilisateur à été créer", 201);
     }
 }
